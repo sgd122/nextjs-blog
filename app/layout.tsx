caecb61fa85f5f0f5e331a5819240ffb36ca8888
Seong Gi Dong <sgd0947@gmail.com>
2024-07-08 20:30:41 +0900

    feat(favicons): updatd a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import 'remark-github-blockquote-alert/alert.css'
 
 import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
+import { SearchConfig, SearchProvider } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 import Footer from '@/components/Footer'
